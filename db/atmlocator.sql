822d52987e0479f10078f2465effe53c51045527
Bibek Shrestha <bibek@yipl.com.np>
2010-12-11 21:24:31 +0545

    basic data for database

---
+++ db/atmlocator.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 11, 2010 at 03:39 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `atmlocator`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `atm`
+--
+
+CREATE TABLE IF NOT EXISTS `atm` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bank_id` int(11) DEFAULT NULL,
+  `atm_network_id` int(11) DEFAULT NULL,
+  `latitude` float DEFAULT NULL,
+  `longitude` float DEFAULT NULL,
+  `descriptive_location` varchar(140) DEFAULT NULL,
+  `languages` varchar(255) DEFAULT NULL,
+  `card_usage` enum('swipe','insert') DEFAULT NULL,
+  `charges` text,
+  `one_time_withdraw_limit` int(11) DEFAULT NULL,
+  `additional_property` varchar(45) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `verification_description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_atm_properties_bank` (`bank_id`),
+  KEY `fk_atm_properties_atm_network1` (`atm_network_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `atm`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `atm_network`
+--
+
+CREATE TABLE IF NOT EXISTS `atm_network` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `accepting_countries` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `atm_network`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank`
+--
+
+CREATE TABLE IF NOT EXISTS `bank` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bank_name` varchar(255) DEFAULT NULL,
+  `identifier` varchar(20) DEFAULT NULL,
+  `daily_withdraw_limit` int(11) DEFAULT NULL,
+  `one_time_withdraw_limit` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `identifier_UNIQUE` (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `bank`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE IF NOT EXISTS `rating` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rating_attribute_id` int(11) NOT NULL,
+  `score` float DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `atm_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_rating_rating_attribute1` (`rating_attribute_id`),
+  KEY `fk_rating_user1` (`user_id`),
+  KEY `fk_rating_atm1` (`atm_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `rating`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating_attribute`
+--
+
+CREATE TABLE IF NOT EXISTS `rating_attribute` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `ulimit` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `rating_attribute`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `role_UNIQUE` (`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `role`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` tinyint(1) DEFAULT '0',
+  `credibility` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `fk_user_roles1` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `user`
+--
+
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `atm`
+--
+ALTER TABLE `atm`
+  ADD CONSTRAINT `fk_atm_properties_bank` FOREIGN KEY (`bank_id`) REFERENCES `bank` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_atm_properties_atm_network1` FOREIGN KEY (`atm_network_id`) REFERENCES `atm_network` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `rating`
+--
+ALTER TABLE `rating`
+  ADD CONSTRAINT `fk_rating_rating_attribute1` FOREIGN KEY (`rating_attribute_id`) REFERENCES `rating_attribute` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_rating_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_rating_atm1` FOREIGN KEY (`atm_id`) REFERENCES `atm` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_user_roles1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
