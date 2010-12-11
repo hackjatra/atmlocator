3e2c63d5fd7d04711f8f84df0ec508aa85d3bc68
Bibek Shrestha <bibek@yipl.com.np>
2010-12-11 14:10:24 +0545

    Added openid test library

--- src/system/application/controllers/welcome.php
+++ src/system/application/controllers/welcome.php
@@ -2,16 +2,40 @@
 
 class Welcome extends Controller {
 
-	function Welcome()
+	function __construct()
 	{
-		parent::Controller();	
+		parent::__construct();
+        parse_str($_SERVER['QUERY_STRING'],$_GET);
 	}
 	
 	function index()
 	{
 		$this->load->view('welcome_message');
 	}
+
+    function openid()
+    {
+        $this->load->view('openidform');
+    }
+
+    function openidlogin()
+    {
+        require_once APPPATH . 'libraries/openid.php';
+        try {
+            $openid = new LightOpenID;
+            if (!$openid->mode) {
+                if (isset($_POST['login'])) {
+                    $openid->identity = 'https://www.google.com/accounts/o8/id';
+                    $openid->required = array('namePerson/first', 'namePerson/last', 'contact/email', 'pref/language');
+                    header('Location: ' . $openid->authUrl());
+                }
+            } else {
+                // Successful login
+                var_dump($_POST); var_dump($_GET);
+            }
+        } catch (Exception $e) {
+            print $e->getMessage();
+        }
+    }
 }
 
-/* End of file welcome.php */
-/* Location: ./system/application/controllers/welcome.php */
