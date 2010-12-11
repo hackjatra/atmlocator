2f5c963e1dd46f3c493efa932a33f1508b968811
Sushil <sushil@satellite.(none)>
2010-12-11 21:06:14 +0545

    added one more utility function in custom DbTableAbstract

--- application/models/DbTable/DbTableAbstract.php
+++ application/models/DbTable/DbTableAbstract.php
@@ -3,7 +3,7 @@
  * returns id of current table based on field and value
  * returns only the first row if more than one row is returned from query
  * @field -> field name of table
- * @value -> where field = value
+ * @value -> value of field (for where query)
  */
 class App_Model_DbTable_DbTableAbstract extends Zend_Db_Table_Abstract{
 	function getIdByField ( $field,$value){
@@ -13,5 +13,20 @@ class App_Model_DbTable_DbTableAbstract extends Zend_Db_Table_Abstract{
 		  if(is_array($result))
 			  return $result[0]->id;
 		  return $result->id;
-		}
+	}
+
+/*
+ * returns row of current table based on field and value
+ * returns only the first row if more than one row is returned from query
+ * @field -> field name of table
+ * @value -> value of field (for where query)
+ */
+	function getByField ( $field,$value){
+		  $result=$this->fetchRow("$field= '$value' "); //order by id?
+		  if($result==null)
+			  return;
+		  if(is_array($result))
+			  return $result[0];
+		  return $result;
+	}
 }
