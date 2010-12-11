ddfc5c7a6cfbdefe01091d49439cb3e6312405fa
Bibek Shrestha <bibek@yipl.com.np>
2010-12-11 17:32:53 +0545

    DbTable User

--- application/models/DbTable/User.php
+++ application/models/DbTable/User.php
@@ -1,10 +1,17 @@
 <?php
-    /**
-    * This is the DbTable class for the User table.
-    */
-    class App_Model_DbTable_User extends Zend_Db_Table_Abstract
+
+/**
+ * This is the DbTable class for the User table.
+ */
+class App_Model_DbTable_User extends Zend_Db_Table_Abstract
+{
+
+    /** Table name */
+    protected $_name = 'user';
+
+    public function findByEmail($email)
     {
-       /** Table name */
-        protected $_name    = 'user';
+        return $this->fetchRow('email = ?', $email);
     }
-        
+}
+
